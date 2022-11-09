db048ca863de53d7d3117d012bb7b742c1671d01
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-11-09 20:39:47 +0900

    Rename Entity table name

--- src/mbtiquestion/entities/mbtiquestion.entity.ts
+++ src/mbtiquestion/entities/mbtiquestion.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('Mbtiquestion')
+@Entity('mbtiquestion')
 export class Mbtiquestion {
 
     @PrimaryGeneratedColumn('increment')
--- src/mbtiresult/entities/mbtiresult.entity.ts
+++ src/mbtiresult/entities/mbtiresult.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('Mbtiresult')
+@Entity('mbtiresult')
 export class Mbtiresult {
 
     @PrimaryGeneratedColumn('increment')
